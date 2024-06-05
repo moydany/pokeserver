2e99ffaba4c679594f6f7e5a697d925f22411c26
Moises Daniel <139163704+moi-eval@users.noreply.github.com>
2024-06-04 22:09:56 -0600

    Add pokemon routes and setup Swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import 'reflect-metadata';
 import express from 'express';
 import { connectDatabase } from './config/database';
 import authRoutes from './routes/authRoutes';
+import pokemonRoutes from './routes/pokemonRoutes';
+import { container } from './inversify.config';
+import { setupSwagger } from './swagger';
+import logger from './config/logger';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,16 +14,20 @@ app.use(express.json());
 
 const dbUri = 'mongodb://mongo:27017/myDB';
 connectDatabase(dbUri)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
+  .then(() => logger.info('Connected to MongoDB'))
+  .catch((err) => {
+    logger.error('Error connecting to MongoDB:', err);
     process.exit(1);
   });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/pokemon', pokemonRoutes);
+
+// Configurar Swagger
+setupSwagger(app);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  logger.info(`Server is running on port ${port}`);
 });
 
 export { app };
