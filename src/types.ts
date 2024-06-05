93de9547d639bbf4a4518178fe4c1048bc4f3bce
Moises Daniel <139163704+moi-eval@users.noreply.github.com>
2024-06-04 22:10:34 -0600

    Add types for dependency injection

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+const TYPES = {
+  PokemonService: Symbol.for('PokemonService'),
+  PokemonRepository: Symbol.for('PokemonRepository'),
+  UserRepository: Symbol.for('UserRepository'),
+  AuthService: Symbol.for('AuthService'),
+};
+
+export { TYPES };
