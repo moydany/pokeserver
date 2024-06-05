e08cb068ae0c60f664c3c4220479265abe8aef5a
Moises Daniel <139163704+moi-eval@users.noreply.github.com>
2024-06-04 23:39:06 -0600

    Improve error handling in database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,12 +2,18 @@ import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
   const uri = process.env.MONGO_URI;
+
   if (!uri) {
     throw new Error('MONGO_URI is not defined in the environment variables');
   }
 
-  await mongoose.connect(uri);
-  console.log('Connected to MongoDB');
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
 };
 
 export { connectDatabase };
